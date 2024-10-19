9b07289f43272ee2349def0a22226ac7083991a7
Atakan Yontar <atakanyontar@gmail.com>
2024-10-19 17:10:20 +0300

    rand session id

--- src/components/new-chat/index.jsx
+++ src/components/new-chat/index.jsx
@@ -26,10 +26,12 @@ const ChatComponent = () => {
         setMessages([...messages, { sender: 'user', text: input }]);
         setInput('');
 
+        const ses_id=Date.now()
+        console.log(ses_id)
         try {
             const body = {
                 "body": "{\"messages\": [{\"role\": \"user\", \"content\": [{\"type\": \"text\", \"text\": \"" + input + "\"}]}]}",
-                "session_id": "abbabasdbsadg"
+                "session_id": ses_id.toString()
             }
             const url = `https://xd424xhfs9.execute-api.us-west-2.amazonaws.com/FinSmart-Stage/process-text`;
 
